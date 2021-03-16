dd28e21bac376885ad7875daa53f3d9f63efb678
djilaliouadah <djilali.ouadah@viacesi.fr>
2021-03-16 20:30:07 +0100

    page acceuil

--- page_de_garde.php
+++ page_de_garde.php
@@ -14,12 +14,29 @@
 <body>
 
   <header>
-    <div class="Acc">
-      <nav class="nav nav-tabs">
-        <a class="nav-link active " href="#">Accueil</a>
-        <a class="nav-link " href="#">Mon compte</a>
-        <a class="nav-link " href="#">Contact</a>
-        <a class="nav-link " href="#">Nos Réseaux</a>
+    <div class="headerheight">
+      <nav class="navbar navbar-expand-lg navbar-light bg-light borderB">
+        <a class="navbar-brand" href="#">Menu</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+              <a class="nav-link" href="#">Accueil<span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#two">Contact</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#three">Politique de confidentialité</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#three">Réseaux sociaux</a>
+            </li>
+          </ul>
+        </div>
       </nav>
     </div>
 
