01046348fad8c05f7df6e0e185789d4a7ad1ced4
djilaliouadah <djilali.ouadah@viacesi.fr>
2021-03-19 10:52:48 +0100

    aaa

--- page_de_garde.php
+++ page_de_garde.php
@@ -8,7 +8,7 @@
 
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css">
   <link rel="stylesheet" href="./assets/vendors/fontawesome/css/all.min.css">
-  <link rel="stylesheet" type="text/css" href="page_de_garde - Copie.css">
+  <link rel="stylesheet" type="text/css" href="page_de_garde.css">
 </head>
 
 <body>
