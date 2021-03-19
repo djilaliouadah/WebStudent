578a37ec9630fefc033031136caa5fe6518fed4e
RabieH1 <73874115+RabieH1@users.noreply.github.com>
2021-03-19 15:52:19 +0100

    15h52 candidatures

---
+++ candidatures.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html>
+    <meta charset='utf-8'>
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Back'Stage</title>
+  
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="./assets/vendors/fontawesome/css/all.min.css">
+    <link rel="stylesheet" type="text/css" href="gerer_les_compte.css">
+<head>
+  <title></title>
+</head>
+<body>
+
+    <header class="header" id="top">
+        <div class="headerheight">
+            <img src="assets/img/back'stage.PNG" alt="" />
+            <h2 id="titre">Back'Stage</h2>
+            <nav class="navbar navbar-expand-lg navbar-light bg-light borderB">
+                <a class="navbar-brand" href="#">Menu</a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse"
+                    data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                    aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav mr-auto">
+                        <li class="nav-item active">
+                            <a class="nav-link" href="page_de_garde.php">Accueil<span class="sr-only">(current)</span></a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#two">Contact</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#three">Politique de confidentialité</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#three">Réseaux sociaux</a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        </div>
+            <br>
+    </header>
+
+    
+
+    
+        <div class="container">
+      
+    <div class="d-flex justify-content-center"><div class="bordure">
+
+       <h3 class="d-flex justify-content-center">Candidatures</h3>
+       <br>
+       <ul>
+
+        <li class="d-flex justify-content-center"><a href="">Wish-List</a></li>
+        <br>
+        <li class="d-flex justify-content-center"><a href="creer_compte.php">Candidatures</a></li>
+        <br>
+        <li class="d-flex justify-content-center"><a href="">Avancement</a></li>
+        
+
+
+       </ul>
+     
+
+
+    </div>
+
+    </div>
+</div>
+<footer class="footer1">
+    <a href="page_de_garde.html">Accueil |</a>
+    <a href="https://developer.mozilla.org/en-US/docs/Web/HTML/Element/header">Présentation |</a>
+    <a href="https://fontawesome.com/icons/linkedin-in?style=brands">Conditions Générales d'Utilisation |</a>
+    <a href="https://fontawesome.com/icons/linkedin-in?style=brands">Politique de confidentialité |</a>
+    <a href="https://developer.mozilla.org/en-US/docs/Web/HTML/Element/header">Cookies |</a>
+    <a href="https://developer.mozilla.org/en-US/docs/Web/HTML/Element/header">Mentions Légales |</a>
+    <a href="https://fontawesome.com/icons/linkedin-in?style=brands">Contact |</a>
+    <a href="https://fontawesome.com/icons/linkedin-in?style=brands">Réseaux Sociaux </a>
+</footer>
+
+    
+  <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+  <script src="./assets/vendors/bootstrap/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
