e9e902f78a016d13e791e789585724deabafe519
RabieH1 <73874115+RabieH1@users.noreply.github.com>
2021-03-22 16:07:46 +0100

    16h07 ouhaaaaaaaaaaaaaaaaaaaa

---
+++ Evaluer_entreprise.php
@@ -0,0 +1,101 @@
+<!DOCTYPE html>
+<html>
+    <meta charset='utf-8'>
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Back'Stage</title>
+  
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="./assets/vendors/fontawesome/css/all.min.css">
+    <link rel="stylesheet" type="text/css" href="Evaluer_entreprise.css">
+<head>
+  <title></title>
+</head>
+<body>
+
+    <header class="header" id="top">
+        <div class="headerheight">
+            <img src="assets/img/back'stage.PNG" alt="" />
+            <h2 id="titre">Back'Stage</h2>
+            <nav class="navbar navbar-expand-lg navbar-light bg-light borderB">
+                <a class="navbar-brand" href="#">Menu</a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse"
+                    data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                    aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav mr-auto">
+                        <li class="nav-item active">
+                            <a class="nav-link" href="page_de_garde.php">Accueil<span class="sr-only">(current)</span></a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#two">Contact</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#three">Politique de confidentialité</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#three">Réseaux sociaux</a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        </div>
+            <br>
+    </header>
+
+    <div id="we">
+<a href="page_de_garde.php">Déconnexion</a>
+</div>
+<center>
+<h1>Evaluer une Entreprise</h1>
+</center>
+
+<form class="wesh">
+
+<div class="form-group row">
+    <label for="inputPassword" class="col-3-3 col-form-label">Nom de l'entreprise :</label>
+    <div class="col-sm-2">
+      <input type="text" class="form-control" id="inputPassword" NAME="output" VALUE="">
+    </div>
+</div>
+</form>
+<div id="rhalis">
+<div class="rating"><!--
+   --><a href="#5" title="Donner 5 étoiles">☆</a><!--
+   --><a href="#4" title="Donner 4 étoiles">☆</a><!--
+   --><a href="#3" title="Donner 3 étoiles">☆</a><!--
+   --><a href="#2" title="Donner 2 étoiles">☆</a><!--
+   --><a href="#1" title="Donner 1 étoile">☆</a>
+</div>
+</div>
+
+<form class="bah">
+<div class="form-group row">
+<label for="inputPassword" class="col-3-7 col-form-label">Commentaire (Falcutatif):
+</label>
+    <div class="col-sm-3">
+      <input type="text" class="form-control" id="inputPassword" NAME="input" VALUE="" style="height:150px;">
+    </div>
+    </div>
+    <div class="ehpa">
+    <div class="col-sm-4">
+    <input type="submit" value="Evaluer l'entreprise">
+</div>
+</div>
+</form>
+
+
+<footer class="footer1">
+    <a href="page_de_garde.html">Accueil |</a>
+    <a href="https://developer.mozilla.org/en-US/docs/Web/HTML/Element/header">Présentation |</a>
+    <a href="https://fontawesome.com/icons/linkedin-in?style=brands">Conditions Générales d'Utilisation |</a>
+    <a href="https://fontawesome.com/icons/linkedin-in?style=brands">Contact |</a>
+    <a href="https://fontawesome.com/icons/linkedin-in?style=brands">Réseaux Sociaux </a>
+</footer>
+
+<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+  <script src="./assets/vendors/bootstrap/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
