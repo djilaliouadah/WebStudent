1a2af1167ba66b2a6496756621bb2192d52cf2fe
djilaliouadah <djilali.ouadah@viacesi.fr>
2021-03-24 12:53:51 +0100

    modod

---
+++ comparaison offre.php
@@ -0,0 +1,96 @@
+﻿<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset='utf-8'>
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Back'Stage</title>
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="./assets/vendors/fontawesome/css/all.min.css">
+    <link rel="stylesheet" type="text/css" href="comparaison.css">
+
+</head>
+
+<body>
+
+    <header class="header" id="top">
+        <div class="headerheight">
+            <img src="assets/img/back'stage.PNG" alt="" />
+            <h2 id="titre">Back'Stage</h2>
+            <nav class="navbar navbar-expand-lg navbar-light bg-light borderB">
+                <a class="navbar-brand" href="#">Menu</a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav mr-auto">
+                        <li class="nav-item active">
+                            <a class="nav-link" href="page_de_garde.php">Accueil<span class="sr-only">(current)</span></a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#two">Contact</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#three">Politique de confidentialité</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#three">Réseaux sociaux</a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+            <div id="we">
+                <a href="page_de_garde.php">Déconnexion</a>
+            </div>
+        </div>
+        <br>
+
+    </header>
+    <h1>comparaison</h1>
+
+
+    <div class="container-xl">
+        <div class="row">
+            <div class="col-6">
+                <fieldset class="carre">
+                    <legend>number 1</legend>
+                    <p>jfdsoigj pghuiopjfiepodnv udpjisv poifjvnud jiqojpfdi i
+                        fhdsui fhsi uisdhofdupsfqu hdsiofh duqspfhudqosi f
+                    </p>
+                </fieldset>
+                <div class="bouton">
+                    <input type="button" name="Envoyer" value="Postuler" class="btn btn-primary">
+                </div>
+
+            </div>
+            <div class="col-6">
+                <fieldset class="carre">
+                    <legend>number 2</legend>
+                    <p>jfdsoigj pghuiopjfiepodnv udpjisv poifjvnud jiqojpfdi i
+                        fhdsui fhsi uisdhofdupsfqu hdsiofh duqspfhudqosi f
+                    </p>
+                </fieldset>
+                <div class="bouton">
+                    <input type="button" name="Envoyer" value="Postuler" class="btn btn-primary">
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+    <footer class="footer1">
+        <a href="page_de_garde.html">Accueil |</a>
+        <a href="https://developer.mozilla.org/en-US/docs/Web/HTML/Element/header">Présentation |</a>
+        <a href="https://fontawesome.com/icons/linkedin-in?style=brands">Conditions Générales d'Utilisation |</a>
+        <a href="https://fontawesome.com/icons/linkedin-in?style=brands">Contact |</a>
+        <a href="https://fontawesome.com/icons/linkedin-in?style=brands">Réseaux Sociaux </a>
+    </footer>
+
+    <script src="./assets/vendors/jquery/jquery-3.5.1.min.js"></script>
+    <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+    <script src="./assets/vendors/bootstrap/js/bootstrap.bundle.min.js"></script>
+</body>
+
+</html>
--- creer_compte.php
+++ creer_compte.php
@@ -9,10 +9,8 @@
         <head>
             <meta charset="utf-8" />
             <meta name="viewport" content="width=device-width, initial-scale=1, shrink-tofit=no">
-            <link rel="stylesheet" href="./assets/vendors/bootstrap/css/bootstrap.min.css">
-            <link rel="stylesheet" href="./assets/vendors/fontawesome/css/all.min.css">
-            <link rel="preconnect" href="https://fonts.gstatic.com">
-            <link href="https://fonts.googleapis.com/css2?family=Happy+Monkey&display=swap" rel="stylesheet">
+            <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="./assets/vendors/fontawesome/css/all.min.css">
             <link rel="stylesheet" type="text/css" href="styleprojet.css">
             <title>Back'Stage</title>
         </head>
