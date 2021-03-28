f39221b3a86318d8e2de34f127ac722978e67ac2
RabieH1 <73874115+RabieH1@users.noreply.github.com>
2021-03-28 20:05:39 +0200

    reseaux

---
+++ reseaux.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-tofit=no">
+    <link rel="stylesheet" href="./css/bootstrap.min.css">
+    <link rel="stylesheet" href="./css/all.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="./assets/vendors/fontawesome/css/all.min.css">
+	<link rel="stylesheet" type="text/css" href="reseaux.css">
+</head>
+<body>
+   <div class="rx">
+<header>
+	<h1>Réseaux Sociaux</h1>
+</header>
+
+<div class="page1">
+<div class="qs"><i class="fab fa-facebook fa-3x"></i></div>
+<div class="qsd"><i class="fab fa-instagram fa-3x"></i></div>
+<div class="qsdf"><i class="fab fa-twitter fa-3x"></i></div>
+<div class="qsdfg"><i class="fab fa-linkedin-in fa-3x"></i></div>
+
+            <div class="az">
+
+            <button onclick="window.location.href = '';"> Back'Stage</button>
+            </div>
+
+
+            <div class="aze">
+            <button onclick="window.location.href = '';"> Back'Stage</button>
+            </div>
+
+            <div class="azer">
+            <button onclick="window.location.href = '';"> Back'Stage</button>
+            </div>
+
+            <div class="azert">
+            <button onclick="window.location.href = '';"> Back'Stage</button>
+            </div>
+</div>
+            
+
+   	<footer class="footer1">
+   		<a href="page_de_garde.html">Accueil |</a>
+   		<a href="https://developer.mozilla.org/en-US/docs/Web/HTML/Element/header">Contact |</a>
+   		<a href="https://fontawesome.com/icons/linkedin-in?style=brands">Politique de confidentialité |</a>
+   		<a href="https://fontawesome.com/icons/linkedin-in?style=brands">Réseaux Sociaux </a>
+   	</footer>
+   	 <link rel="stylesheet" href="C:\Users\Rabie\OneDrive\Bureau\2eme_année_cesi\Projet Web\www\assets\vendors\bootstrap\bootstrap-4.5.3-dist\css\bootstrap.min.css">
+   	<link rel="stylesheet"  href="C:\Users\Rabie\OneDrive\Bureau\2eme_année_cesi\Projet Web\www\assets\vendors\fontawesome\css\all.min.css">
+
+      </div>
+</body>
+</html>
